347f85973d90c103bc09d5b9d1d12955a6d65366
RodrigoAnciaes <81170574+RodrigoAnciaes@users.noreply.github.com>
2025-02-20 19:14:03 -0300

    Slightly more readble Hellscape.

--- ETL_project/bin/main.ml
+++ ETL_project/bin/main.ml
@@ -1,51 +1,45 @@
-
-
 type result_record = {
   total_amount: float; (* quantity * price *)
   total_tax: float;   (* total_amount * tax *)
 };;
 
 let transform_to_result orders order_items =
-  let order_item_map = Hashtbl.create 100 in
-  List.iter (fun order_item ->
+  (* Compute a list of (order_id, total_amount, total_tax) records from order_items *)
+  let item_results = List.map (fun order_item ->
     let order_id = order_item.Read_data.order_id in
     let total_amount = order_item.Read_data.price *. (float_of_int order_item.Read_data.quantity) in
     let total_tax = total_amount *. order_item.Read_data.tax in
-    let result_record = { total_amount; total_tax } in
-    let result_records =
-      try Hashtbl.find order_item_map order_id
-      with Not_found -> [] in
-    Hashtbl.replace order_item_map order_id (result_record :: result_records)
-  ) order_items;
+    (order_id, { total_amount; total_tax })
+  ) order_items in
 
+  (* Group by order_id and accumulate total amounts and taxes *)
+  let grouped_results = List.fold_left (fun acc (order_id, record) ->
+    let updated_list =
+      match List.assoc_opt order_id acc with
+      | Some existing_list -> (record :: existing_list)
+      | None -> [record]
+    in
+    (order_id, updated_list) :: (List.remove_assoc order_id acc)
+  ) [] item_results in
+
+  (* Merge grouped results to compute final totals *)
   List.map (fun order ->
     let order_id = order.Read_data.id in
-    let result_records =
-      try Hashtbl.find order_item_map order_id
-      with Not_found -> [] in
+    let result_records = match List.assoc_opt order_id grouped_results with
+      | Some records -> records
+      | None -> [] in
     let total_amount = List.fold_left (fun acc record -> acc +. record.total_amount) 0.0 result_records in
     let total_tax = List.fold_left (fun acc record -> acc +. record.total_tax) 0.0 result_records in
     (order_id, total_amount, total_tax)
   ) orders;;
-
   
-
-
-
-
-
-
+(* Main Execution *)
 let () =
-  Printf.printf "Hello, world!\n";;
+  Printf.printf "Hello, world!\n";
   let orders = Read_data.read_order_data "data/order.csv" in
   let order_items = Read_data.read_order_item_data "data/order_item.csv" in
   let results = transform_to_result orders order_items in
   List.iter (fun (order_id, total_amount, total_tax) ->
     Printf.printf "order_id: %d, total_amount: %f, total_tax: %f\n" order_id total_amount total_tax
-  ) results;;
+  ) results;
   Printf.printf "Goodbye, world!\n";;
-
-
-
-
-
